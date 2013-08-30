eb7ccc955611ed4d461ff6d32b5b3499919f38ab
Brett Bittrick <brett.bittrick@gmail.com>
2013-08-30 09:55:32 -0700

    Nothing

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os.path
+import sys
+
 print "Irc Log parser for use in limechat"
+
+
+for fileName in os.listdir(str(raw_input("Enter path: "))):
+	print fileName
+	if os.path.isfile(fileName):
+		curFile = open(fileName)
+		print(curFile.readline())
+		curFile.close()
+	else :
+		print("%s is a directory" % fileName)
